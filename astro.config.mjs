891fb558c0ee07abaf8351aaa004a24bd849768c
Justin Hall <justin.r.hall@gmail.com>
2025-07-26 10:42:36 -0600

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: vercel({
+    experimentalStaticHeaders: true,
+  }),
+  experimental: {
+    csp: true,
+  },
+});
