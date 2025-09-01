06f870e055d2a54eccde3bee949f7f2d16c91448
Justin Hall <justin.r.hall@gmail.com>
2025-09-01 12:38:41 -0600

    Remove Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwind()],
-  },
   adapter: vercel({
     experimentalStaticHeaders: true,
   }),
